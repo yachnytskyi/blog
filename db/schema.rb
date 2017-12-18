ad3623d3684301a58e1bf8172aed0f2560d03aab
yachnytskyi <yachnytskyi@gmail.com>
2017-12-18 16:14:29 +0300

    Add to upload image on Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212084753) do
+ActiveRecord::Schema.define(version: 20171218125231) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "summary"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "posts" because of following StandardError
+#   Unknown type 'strring' for column 'image'
 
 end
