1e2116bdcf15481db1b132edc8416668517f88bf
yachnytskyi <yachnytskyi1992@gmail.com>
2018-09-02 22:48:09 +0300

    Added pagination

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick'
 gem 'trix'
+gem 'will_paginate', '~> 3.1.0'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
     def index
-        @posts = Post.all
+        @posts = Post.paginate(page: params[:page], per_page: 3)
     end
 
     def show
@@ -19,7 +19,8 @@ class PostsController < ApplicationController
         if @post.save
             redirect_to @post, success: 'Статья создана'
         else
-            render :new, danger: 'Статья не создана'
+            flash.now[:danger] = 'Статья не создана'
+            render :new
         end
     end
 
@@ -31,7 +32,8 @@ class PostsController < ApplicationController
         if @post.update_attributes(post_params)
             redirect_to @post, success: 'Статья успешно обновлена'
         else
-            render :edit, danger: 'Статья не обновлена'
+            flash.now[:danger] = 'Статья не обновлена'
+            render :edit
         end
     end
 
