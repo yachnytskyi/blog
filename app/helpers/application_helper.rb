d609156bf8ddd082438154badc8f2a70706db6f3
yachnytskyi <yachnytskyi1992@gmail.com>
2018-09-06 14:14:26 +0300

    Added Authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ end
     result << content_tag(:li, category.name, class: "breadcrumb-item active")
     result.join('').html_safe
   end
+
+  def bootstrap_class_for(name)
+    {
+      success: "alert-success",
+      error: "alert-danger",
+      danger: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info",
+
+    }[name.to_sym] || name
 end
+end 
